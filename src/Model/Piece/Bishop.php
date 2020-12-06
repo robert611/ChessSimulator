9c447310cf0840087c2cd5ce6052f95ca2613185
robert611 <cyrkiel86@interia.pl>
2020-12-06 19:57:29 +0100

    Change way of calculating possible moves for bishop to the one of the rook

--- src/Model/Piece/Bishop.php
+++ src/Model/Piece/Bishop.php
@@ -44,142 +44,84 @@ class Bishop extends Piece
 			$cords[0] is horizontal column
 			$cords[1] is vertical column
 		*/
-		
-		/* [up, left] diagonal */
-		for($horizontalSquareNumber = $this->cords[0] + 1, $iteration = 1; $horizontalSquareNumber <= 8; $horizontalSquareNumber++, $iteration++)
+
+		$potentialMoves = $this->getPotentialMovesCoordinates();
+
+		/* Potential moves are sorted by direction in its own arrays with names [left, up, right, down] */
+		foreach ($potentialMoves as $direction)
 		{
-			/* Going up left we will decrease our position on vertical column, and increase on horizontal column */
-
-			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] - $iteration)) break;
-
-			/* These are coordination we check in current loop cycle if bishop can move there */
-			$checkedCoordination = [$horizontalSquareNumber, $this->cords[1] - $iteration];
-			$pieceOnSquare = $board[$horizontalSquareNumber][$this->cords[1] - $iteration]->getPiece();
-
-			/* If in a given coordinates is placed an opponent piece, then add it to possible moves and break since we can't go further on this diagonal */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() !== $this->getSide())
+			foreach ($direction as $move)
 			{
-				$possibleMoves[] = $checkedCoordination;
-				break;
+				/* These are coordination we check in current loop cycle if bishop can move there */
+				$pieceOnSquare = $board[$move[0]][$move[1]]->getPiece();
+
+				/* If in a given coordinates is placed an opponent piece, then add it to possible moves and break since we can't go further on this diagonal */
+				if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() !== $this->getSide())
+				{
+					$possibleMoves[] = $move;
+					break;
+				}
+
+				if ($pieceOnSquare == null)
+				{
+					$possibleMoves[] = $move;
+				}
+
+				/* In this case we try to hit our own piece, so we can't go this way or any further on this diagonal, and it means we are protecting that piece */
+				if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() == $this->getSide()) {
+					$protectedSquares[] = $move;
+					break;
+				}
 			}
-
-			if ($pieceOnSquare == null)
-			{
-				$possibleMoves[] = $checkedCoordination;
-			}
-
-			/* In this case we try to hit our own piece, so we can't go this way or any further on this diagonal, and it means we are protecting that piece */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() == $this->getSide()) {
-				$protectedSquares[] = $checkedCoordination;
-				break;
-			}
-
-			/* It will apply if we are on A8, C8, E8, G8, A6, A4, A2 position then you can't go further up, because it is the end of a board */
-			if ($horizontalSquareNumber == 1 or $this->cords[1] - $iteration == 8) break; 
-		}
-
-		/* [down, right] diagonal */
-		for($horizontalSquareNumber = $this->cords[0] - 1, $iteration = 1; $horizontalSquareNumber >= 1; $horizontalSquareNumber--, $iteration++)
-		{
-			/* Going down left we will increase our position on vertical column, and decrease on horizontal column */
-
-			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] + $iteration)) break;
-
-			/* These are coordination we check in current loop cycle if bishop can move there */
-			$checkedCoordination = [$horizontalSquareNumber, $this->cords[1] + $iteration];
-			$pieceOnSquare = $board[$horizontalSquareNumber][$this->cords[1] + $iteration]->getPiece();
-
-			/* If in a given coordinates is placed an opponent piece, then add it to possible moves and break since we can't go further on this diagonal */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() !== $this->getSide())
-			{
-				$possibleMoves[] = $checkedCoordination;
-				break;
-			}
-
-			if ($pieceOnSquare == null)
-			{
-				$possibleMoves[] = $checkedCoordination;
-			}
-
-			/* In this case we try to hit our own piece, so we can't go this way or any further on this diagonal, and it means we are protecting that piece */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() == $this->getSide()) {
-				$protectedSquares[] = $checkedCoordination;
-				break;
-			}
-
-			/* It will apply if we are on B1, D1, F1, H1, H3, H5, H7 position then you can't go further up, because it is the end of a board */
-			if ($horizontalSquareNumber == 1 or $this->cords[1] + $iteration == 1) break; 
-		}
-
-		/* [up, right] diagonal */
-		for($horizontalSquareNumber = $this->cords[0] + 1, $iteration = 1; $horizontalSquareNumber <= 8; $horizontalSquareNumber++, $iteration++)
-		{
-			/* Going up right we will increase our position on vertical column, and increase on horizontal column */
-			
-			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] + $iteration)) break;
-
-			/* These are coordination we check in current loop cycle if bishop can move there */
-			$checkedCoordination = [$horizontalSquareNumber, $this->cords[1] + $iteration];
-			$pieceOnSquare = $board[$horizontalSquareNumber][$this->cords[1] + $iteration]->getPiece();
-
-			/* If in a given coordinates is placed an opponent piece, then add it to possible moves and break since we can't go further on this diagonal */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() !== $this->getSide())
-			{
-				$possibleMoves[] = $checkedCoordination;
-				break;
-			}
-
-			if ($pieceOnSquare == null)
-			{
-				$possibleMoves[] = $checkedCoordination;
-			}
-
-			/* In this case we try to hit our own piece, so we can't go this way or any further on this diagonal, and it means we are protecting that piece */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() == $this->getSide()) {
-				$protectedSquares[] = $checkedCoordination;
-				break;
-			}
-
-			/* It will apply if we are on A8, C8, E8, G8, H7, H5, H3, H1 position then you can't go further up, because it is the end of a board */
-			if ($horizontalSquareNumber == 8 or $this->cords[1] + $iteration == 8) break; 
-		}
-
-		/* [down, left] diagonal */
-		for($horizontalSquareNumber = $this->cords[0] - 1, $iteration = 1; $horizontalSquareNumber >= 1; $horizontalSquareNumber--, $iteration++)
-		{
-			/* Going down left we will decrease our position on vertical column, and decrease on horizontal column */
-
-			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] - $iteration)) break;
-
-			/* These are coordination we check in current loop cycle if bishop can move there */
-			$checkedCoordination = [$horizontalSquareNumber, $this->cords[1] - $iteration];
-			$pieceOnSquare = $board[$horizontalSquareNumber][$this->cords[1] - $iteration]->getPiece();
-
-			/* If in a given coordinates is placed an opponent piece, then add it to possible moves and break since we can't go further on this diagonal */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() !== $this->getSide())
-			{
-				$possibleMoves[] = $checkedCoordination;
-				break;
-			}
-
-			if ($pieceOnSquare == null)
-			{
-				$possibleMoves[] = $checkedCoordination;
-			}
-
-			/* In this case we try to hit our own piece, so we can't go this way or any further on this diagonal, and it means we are protecting that piece */
-			if (is_object($pieceOnSquare) && $pieceOnSquare->getSide() == $this->getSide()) {
-				$protectedSquares[] = $checkedCoordination;
-				break;
-			}
-
-			/* It will apply if we are on B1, D1, F1, H1, A2, A4, A6, A8 position then you can't go further up, because it is the end of a board */
-			if ($horizontalSquareNumber == 1 or $this->cords[1] - $iteration == 1) break; 
 		}
 		
 		/* $protectedSquares on its own contains squares to which we can move and those we aim on which are either ours or our oponnent pieces, $possibleMoves contains all of the other moves like empty squares or oponnent pieces*/
 		return ['possible_moves' => $possibleMoves, 'protected_squares' => array_merge($possibleMoves, $protectedSquares)];
 	}
+
+	public function getPotentialMovesCoordinates(): array
+	{
+        /*
+            $cords[0] is horizontal column
+            $cords[1] is vertical column
+        */
+
+		$potentialMoves = ['up_and_left' => [], 'up_and_right' => [], 'down_and_left' => [], 'down_and_right' => []];
+		
+		/* [up, left] diagonal */
+		for ($horizontalSquareNumber = $this->cords[0] + 1, $iteration = 1; $horizontalSquareNumber <= 8; $horizontalSquareNumber++, $iteration++)
+		{			
+			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] - $iteration)) break;
+
+			$potentialMoves['up_and_left'][] = [$horizontalSquareNumber, $this->cords[1] - $iteration];
+		}
+
+		/* [down, right] diagonal */
+		for ($horizontalSquareNumber = $this->cords[0] - 1, $iteration = 1; $horizontalSquareNumber >= 1; $horizontalSquareNumber--, $iteration++)
+		{
+			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] + $iteration)) break;
+
+			$potentialMoves['down_and_right'][] = [$horizontalSquareNumber, $this->cords[1] + $iteration];
+		}
+
+		/* [up, right] diagonal */
+		for ($horizontalSquareNumber = $this->cords[0] + 1, $iteration = 1; $horizontalSquareNumber <= 8; $horizontalSquareNumber++, $iteration++)
+		{
+			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] + $iteration)) break;
+
+			$potentialMoves['up_and_right'][] = [$horizontalSquareNumber, $this->cords[1] + $iteration];
+		}
+		
+		/* [down, left] diagonal */
+		for ($horizontalSquareNumber = $this->cords[0] - 1, $iteration = 1; $horizontalSquareNumber >= 1; $horizontalSquareNumber--, $iteration++)
+		{
+			if (!$this->checkIfCoordinatesAreInsideOfBoard($horizontalSquareNumber, $this->cords[1] - $iteration)) break;
+
+			$potentialMoves['down_and_left'][] = [$horizontalSquareNumber, $this->cords[1] - $iteration];
+        }
+
+		return $potentialMoves;
+    }
 
 	public function getPicture(): string
 	{
