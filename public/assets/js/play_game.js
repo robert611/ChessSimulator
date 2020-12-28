dff3d3c40e10f1bee7aa565f387101b6909ea8b6
robert611 <cyrkiel86@interia.pl>
2020-12-28 16:09:59 +0100

    Fix bugs in play_game.js, now it's possible to play multiple games without reloading page and start from any starting position of the given game not from standard chess starting position.

--- public/assets/js/play_game.js
+++ public/assets/js/play_game.js
@@ -7,6 +7,8 @@ function playGame()
         .then(response => response.json())
         .then(response => JSON.parse(response))
         .then(response => {
+            resetBoardToGameStartingPosition(response);
+
             playNextMove(response);
         });
 }
@@ -25,33 +27,81 @@ function playNextMove(game)
 
         let move = game.moves[moveNumber];
 
-        let squaresWithTheCordOne = document.querySelectorAll(`[data-CordOne="${move.previous_cords[0]}"]`);
-        let newSquaresWithTheCordOne = document.querySelectorAll(`[data-CordOne="${move.new_cords[0]}"]`);
+        let newCords = move.new_cords_square.cords;
 
-        let squareWithThePiece = null;
-        let newSquareForThePiece = null;
-
-        for (square in squaresWithTheCordOne) {
-            if (squaresWithTheCordOne[square].getAttribute('data-CordTwo') == move.previous_cords[1]) {
-                squareWithThePiece = squaresWithTheCordOne[square];
-                break;
-            }
-        }
-
-        for (square in newSquaresWithTheCordOne) {
-            if (newSquaresWithTheCordOne[square].getAttribute('data-CordTwo') == move.new_cords[1]) {
-                newSquareForThePiece = newSquaresWithTheCordOne[square];
-                break;
-            }
-        }
+        let squareWithThePiece = document.getElementsByClassName(`cord-${move.previous_cords[0]}${move.previous_cords[1]}`)[0];
+        let newSquareForThePiece = document.getElementsByClassName(`cord-${newCords[0]}${newCords[1]}`)[0];
 
         squareWithThePiece.firstChild.classList.add('opacity-0');
         newSquareForThePiece.firstChild.classList.remove('opacity-0');
         newSquareForThePiece.firstChild.setAttribute('src', `/assets/images/${move.piece.picture}`);
 
-
         counter.value = moveNumber + 1;
     });
+}
+
+function resetBoardToGameStartingPosition(game)
+{
+    document.getElementById('move-counter').value = "0";
+
+    recreatePlayNextMoveButton();
+
+    /* First recreate board to last move position and then go from last move to first so it will be starting position */
+    for (let i = 1; i <= 8; i++) {
+        for (let j = 1; j <= 8; j++) {
+            let piece = game.board[i][j].piece;
+
+            let squaresWithThisPiece = document.getElementsByClassName(`cord-${i}${j}`)[0];
+
+            if (piece != null)
+            {
+                squaresWithThisPiece.firstChild.classList.remove('opacity-0');
+                squaresWithThisPiece.firstChild.setAttribute('src', `/assets/images/${piece.picture}`)
+            }
+            else 
+            {
+                squaresWithThisPiece.firstChild.classList.add('opacity-0');
+                squaresWithThisPiece.firstChild.setAttribute('src', `/assets/images/black-pawn.png`)
+            }
+        }
+    }
+
+    let movesLength = game.moves.length;
+
+    for (i in game.moves)
+    {
+        let move = game.moves[movesLength - (parseInt(i) + 1)];
+
+        let newCords = move.new_cords_square.cords;
+
+        let squareWithThePiece = document.getElementsByClassName(`cord-${newCords[0]}${newCords[1]}`)[0];
+        let newSquareForThePiece = document.getElementsByClassName(`cord-${move.previous_cords[0]}${move.previous_cords[1]}`)[0];
+
+        if (move.new_cords_square.piece != null) {
+            squareWithThePiece.firstChild.setAttribute('src', `/assets/images/${move.new_cords_square.piece.picture}`);
+        } else {
+            squareWithThePiece.firstChild.classList.add('opacity-0');
+        }
+
+        newSquareForThePiece.firstChild.classList.remove('opacity-0');
+        newSquareForThePiece.firstChild.setAttribute('src', `/assets/images/${move.piece.picture}`);
+    }
+}
+
+function recreatePlayNextMoveButton()
+{
+    playNextMoveButton.remove();
+
+    let container = playGameButton.parentElement;
+
+    let newButton = document.createElement('button');
+    newButton.setAttribute('class', 'btn btn-primary');
+    newButton.setAttribute('id', 'play-next-move-button');
+    newButton.textContent = "Play next move";
+
+    container.appendChild(newButton);
+
+    playNextMoveButton = document.getElementById('play-next-move-button');
 }
 
 playGameButton.addEventListener('click', playGame);
